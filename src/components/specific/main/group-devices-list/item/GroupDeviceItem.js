8c7c820c40220ea241c118dd7f8eb3a6a861cfe8
IncineratorG <alexander.v.dorohov@yandex.ru>
2021-09-14 19:11:35 +0300

    Работа над сервисом.

---
+++ src/components/specific/main/group-devices-list/item/GroupDeviceItem.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const GroupDeviceItem = ({item}) => {
+  return (
+    <View style={[styles.itemContainer, {backgroundColor: item.code}]}>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Text style={styles.itemCode}>{item.code}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: 100,
+    width: 200,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 150,
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
+  },
+});
+
+export default React.memo(GroupDeviceItem);
