7e517973c834fb814c768353ef8c63085a51ef0b
Alexander <alexander.v.dorohov@yandex.ru>
2021-09-24 20:35:52 +0300

    Работа над приложением.

--- src/components/specific/group/image-viewer-modal/ImageViewerModal.js
+++ src/components/specific/group/image-viewer-modal/ImageViewerModal.js
@@ -5,6 +5,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {SystemEventsHandler} from '../../../../utils/common/system-events-handler/SystemEventsHandler';
 
 const ImageViewerModal = ({visible, image, onClose}) => {
+  const [internalVisible, setInternalVisible] = useState(false);
   const [imagesData, setImagesData] = useState([]);
   const [viewerInitialIndex, setViewerInitialIndex] = useState(-2);
 
@@ -26,39 +27,108 @@ const ImageViewerModal = ({visible, image, onClose}) => {
     SystemEventsHandler.onInfo({info: 'CHANGE_IMAGE'});
   }, []);
 
-  // useEffect(() => {
-  //   SystemEventsHandler.onInfo({info: 'WILL_SETTTTT'});
-  //
-  //   if (visible) {
-  //     setViewerInitialIndex(0);
-  //   } else {
-  //     setViewerInitialIndex(-1);
-  //   }
-  // }, [visible]);
-
   useEffect(() => {
-    SystemEventsHandler.onInfo({info: 'ImageViewerModal->useEffect()'});
+    SystemEventsHandler.onInfo({info: 'ImageViewerModal->useEffect()|2|'});
 
-    if (!image) {
+    if (!visible) {
+      setInternalVisible(false);
       return;
     }
 
-    const imageObject = {
-      url: 'data:image/jpg;base64,' + image,
-    };
+    if (image && visible) {
+      const imageObject = {
+        url: 'data:image/jpg;base64,' + image,
+      };
 
-    setViewerInitialIndex(0);
+      const imagesArray = [];
+      imagesArray.push(imageObject);
 
-    const imagesArray = [];
-    imagesArray.push(imageObject);
-    setImagesData(imagesArray);
+      setImagesData(imagesArray);
+      setViewerInitialIndex(0);
 
-    // setViewerInitialIndex(0);
-  }, [image]);
+      SystemEventsHandler.onInfo({
+        info: 'ImageViewerModal->useEffect(): IMAGES_PRE_SET: 0',
+      });
+
+      setTimeout(() => {
+        setImagesData([]);
+        setViewerInitialIndex(0);
+
+        SystemEventsHandler.onInfo({
+          info: 'ImageViewerModal->useEffect(): IMAGES_PRE_SET: 1',
+        });
+
+        setTimeout(() => {
+          const otherImagesArray = [];
+          otherImagesArray.push(imageObject);
+
+          setImagesData(otherImagesArray);
+          setViewerInitialIndex(0);
+          setInternalVisible(true);
+
+          SystemEventsHandler.onInfo({
+            info: 'ImageViewerModal->useEffect(): IMAGES_PRE_SET: 2',
+          });
+        }, 100);
+      }, 200);
+    }
+  }, [image, visible]);
+
+  // useEffect(() => {
+  //   SystemEventsHandler.onInfo({info: 'ImageViewerModal->useEffect()'});
+  //
+  //   if (!image) {
+  //     // setViewerInitialIndex(-1);
+  //     // setImagesData([]);
+  //     return;
+  //   }
+  //
+  //   const imageObject = {
+  //     url: 'data:image/jpg;base64,' + image,
+  //   };
+  //
+  //   setViewerInitialIndex(0);
+  //
+  //   const imagesArray = [];
+  //   imagesArray.push(imageObject);
+  //   setImagesData(imagesArray);
+  //
+  //   SystemEventsHandler.onInfo({
+  //     info: 'ImageViewerModal->useEffect(): IMAGES_SET',
+  //   });
+  //
+  //   // setViewerInitialIndex(0);
+  // }, [image]);
+
+  // useEffect(() => {
+  //   SystemEventsHandler.onInfo({info: 'ImageViewerModal->useEffect()'});
+  //
+  //   if (!image) {
+  //     // setViewerInitialIndex(-1);
+  //     // setImagesData([]);
+  //     return;
+  //   }
+  //
+  //   const imageObject = {
+  //     url: 'data:image/jpg;base64,' + image,
+  //   };
+  //
+  //   setViewerInitialIndex(0);
+  //
+  //   const imagesArray = [];
+  //   imagesArray.push(imageObject);
+  //   setImagesData(imagesArray);
+  //
+  //   SystemEventsHandler.onInfo({
+  //     info: 'ImageViewerModal->useEffect(): IMAGES_SET',
+  //   });
+  //
+  //   // setViewerInitialIndex(0);
+  // }, [image]);
 
   return (
     <Modal
-      visible={visible}
+      visible={internalVisible}
       transparent={false}
       onShow={onShowHandler}
       onRequestClose={requestCloseHandler}>
@@ -138,3 +208,166 @@ const styles = StyleSheet.create({
 });
 
 export default ImageViewerModal;
+
+// import React, {useState, useCallback, useEffect} from 'react';
+// import {View, StyleSheet, Modal, TouchableOpacity} from 'react-native';
+// import {ImageViewer} from 'react-native-image-zoom-viewer';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+// import {SystemEventsHandler} from '../../../../utils/common/system-events-handler/SystemEventsHandler';
+//
+// const ImageViewerModal = ({visible, image, onClose}) => {
+//   const [imagesData, setImagesData] = useState([]);
+//   const [viewerInitialIndex, setViewerInitialIndex] = useState(-2);
+//
+//   const onShowHandler = useCallback(() => {}, []);
+//
+//   const arrowBackHandler = useCallback(() => {
+//     if (onClose) {
+//       onClose();
+//     }
+//   }, [onClose]);
+//
+//   const requestCloseHandler = useCallback(() => {
+//     if (onClose) {
+//       onClose();
+//     }
+//   }, [onClose]);
+//
+//   const changeImageHandler = useCallback(async (index) => {
+//     SystemEventsHandler.onInfo({info: 'CHANGE_IMAGE'});
+//   }, []);
+//
+//   useEffect(() => {
+//     SystemEventsHandler.onInfo({info: 'ImageViewerModal->useEffect()'});
+//
+//     if (!image) {
+//       // setViewerInitialIndex(-1);
+//       // setImagesData([]);
+//       return;
+//     }
+//
+//     const imageObject = {
+//       url: 'data:image/jpg;base64,' + image,
+//     };
+//
+//     setViewerInitialIndex(0);
+//
+//     const imagesArray = [];
+//     imagesArray.push(imageObject);
+//     setImagesData(imagesArray);
+//
+//     SystemEventsHandler.onInfo({
+//       info: 'ImageViewerModal->useEffect(): IMAGES_SET',
+//     });
+//
+//     // setViewerInitialIndex(0);
+//   }, [image]);
+//
+//   // useEffect(() => {
+//   //   SystemEventsHandler.onInfo({info: 'ImageViewerModal->useEffect()'});
+//   //
+//   //   if (!image) {
+//   //     // setViewerInitialIndex(-1);
+//   //     // setImagesData([]);
+//   //     return;
+//   //   }
+//   //
+//   //   const imageObject = {
+//   //     url: 'data:image/jpg;base64,' + image,
+//   //   };
+//   //
+//   //   setViewerInitialIndex(0);
+//   //
+//   //   const imagesArray = [];
+//   //   imagesArray.push(imageObject);
+//   //   setImagesData(imagesArray);
+//   //
+//   //   SystemEventsHandler.onInfo({
+//   //     info: 'ImageViewerModal->useEffect(): IMAGES_SET',
+//   //   });
+//   //
+//   //   // setViewerInitialIndex(0);
+//   // }, [image]);
+//
+//   return (
+//     <Modal
+//       visible={visible}
+//       transparent={false}
+//       onShow={onShowHandler}
+//       onRequestClose={requestCloseHandler}>
+//       <ImageViewer
+//         imageUrls={imagesData}
+//         onCancel={onClose}
+//         useNativeDriver={false}
+//         index={viewerInitialIndex}
+//         enablePreload={true}
+//         onChange={changeImageHandler}
+//         renderHeader={(currentIndex) => {
+//           const shareImageHandler = () => {};
+//
+//           const removeImageHandler = () => {};
+//
+//           // return (
+//           //   <View style={{width: 100, height: 50, backgroundColor: 'red'}} />
+//           // );
+//
+//           return (
+//             <View style={styles.mainContainer}>
+//               <TouchableOpacity onPress={arrowBackHandler}>
+//                 <View style={styles.arrowBackContainer}>
+//                   <Icon name="arrow-back" size={26} color={'#ffffff'} />
+//                 </View>
+//               </TouchableOpacity>
+//               <View style={styles.freeSpace} />
+//               <TouchableOpacity onPress={shareImageHandler}>
+//                 <View style={styles.shareImageContainer}>
+//                   <Icon name="share" size={26} color={'#ffffff'} />
+//                 </View>
+//               </TouchableOpacity>
+//               <TouchableOpacity onPress={removeImageHandler}>
+//                 <View style={styles.removeImageContainer}>
+//                   <Icon name="delete" size={26} color={'#ffffff'} />
+//                 </View>
+//               </TouchableOpacity>
+//             </View>
+//           );
+//         }}
+//       />
+//     </Modal>
+//   );
+// };
+//
+// const styles = StyleSheet.create({
+//   mainContainer: {
+//     height: 50,
+//     alignSelf: 'stretch',
+//     flexDirection: 'row',
+//   },
+//   arrowBackContainer: {
+//     height: 50,
+//     width: 50,
+//     // backgroundColor: '#00000066',
+//     backgroundColor: 'black',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   freeSpace: {
+//     flex: 1,
+//   },
+//   shareImageContainer: {
+//     height: 50,
+//     width: 50,
+//     backgroundColor: '#00000066',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   removeImageContainer: {
+//     height: 50,
+//     width: 50,
+//     backgroundColor: '#00000066',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+//
+// export default ImageViewerModal;
