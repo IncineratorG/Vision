6a2fbc294a9fb2639e6a9117486181168d397d40
IncineratorG <alexander.v.dorohov@yandex.ru>
2021-11-09 21:27:18 +0300

    Работа над сервисом.

--- android/app/src/main/java/com/vision/services/surveillance/data/service_internal/tasks/tasks/foreground_service/stop_foreground_service/StopForegroundServiceTask.java
+++ android/app/src/main/java/com/vision/services/surveillance/data/service_internal/tasks/tasks/foreground_service/stop_foreground_service/StopForegroundServiceTask.java
@@ -72,7 +72,7 @@ public class StopForegroundServiceTask implements ServiceSyncTask {
                 mCurrentGroupName,
                 mCurrentGroupPassword,
                 mCurrentDeviceName,
-                mCurrentServiceMode,
+                mInternalData.mCurrentServiceMode,
                 mOnSuccess,
                 mOnError
         ).run(null);
