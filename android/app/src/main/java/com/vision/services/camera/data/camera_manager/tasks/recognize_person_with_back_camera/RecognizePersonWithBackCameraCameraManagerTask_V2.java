ec2064b7c948d6d28dba1c9daeb7ceef816a4e9f
IncineratorG <alexander.v.dorohov@yandex.ru>
2021-11-10 15:48:32 +0300

    Работа над сервисом.

--- android/app/src/main/java/com/vision/services/camera/data/camera_manager/tasks/recognize_person_with_back_camera/RecognizePersonWithBackCameraCameraManagerTask_V2.java
+++ android/app/src/main/java/com/vision/services/camera/data/camera_manager/tasks/recognize_person_with_back_camera/RecognizePersonWithBackCameraCameraManagerTask_V2.java
@@ -66,7 +66,7 @@ public class RecognizePersonWithBackCameraCameraManagerTask_V2 implements Camera
     }
 
     private void processImage(CameraPreviewFrameData previewImageData) {
-        Log.d("tag", "RecognizePersonWithBackCameraCameraManagerTask_V2->processImage()");
+        Log.d("tag", "RecognizePersonWithBackCameraCameraManagerTask_V2->processImage()->ROTATION_ANGLE: " + mImageRotationDeg);
 
         if (previewImageData == null || !previewImageData.hasImage() || mContext == null) {
             return;
